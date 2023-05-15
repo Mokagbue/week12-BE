0285c6dffb8ee09687ee2055e8751d6ac8aef81e
Michelle Okagbue <umbralcompendium@gmail.com>
2023-05-15 00:22:39 -0400

    debug index

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const app = express();
-const PORT = 8080;       
+// const express = require("express");
+// const app = express();
+// const PORT = 8080;       
 
-app.use(express.json());
-//Middleware that every request that comes in will go through this 
-//express json middleaer which will convert it to json allowing our
-//callbacks to read the information
+// app.use(express.json());
+// //Middleware that every request that comes in will go through this 
+// //express json middleaer which will convert it to json allowing our
+// //callbacks to read the information
 
-app.listen(PORT, ()=> console.log(`running on: ${PORT}`))
+// app.listen(PORT, ()=> console.log(`running on: ${PORT}`))
 
-app.get('/magic', (req, res) => {
-    res.status(200).send({
-        tag: "it's magic baby!"
-    })
-})
+// app.get('/magic', (req, res) => {
+//     res.status(200).send({
+//         tag: "it's magic baby!"
+//     })
+// })
 
-app.post('/magic/:id', (req, res) =>{
-    const {id} =req.params;
-    const {emoji} = req.body;
+// app.post('/magic/:id', (req, res) =>{
+//     const {id} =req.params;
+//     const {emoji} = req.body;
 
-    if(!emoji){
-        res.status(418).send({message: 'An Emoji is required.'})
-    }
-    res.send({
-        emoji:`(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ yay! ${emoji} is ID ${id}`,
-    })
-})
+//     if(!emoji){
+//         res.status(418).send({message: 'An Emoji is required.'})
+//     }
+//     res.send({
+//         emoji:`(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ yay! ${emoji} is ID ${id}`,
+//     })
+// })
